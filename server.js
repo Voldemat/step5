7d4282e733d7e8d9b5253e99594e3608f6efe2ba
unknown <viocan2005@gmail.com>
2021-11-13 14:55:55 +0300

    'add_product_section'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const http = require("http");
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 3000;
 const app = express()
 app.use(express.static("static"))
 
